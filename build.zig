9c8eed6f6fa097f9b35602e55e51c75fd83d92e1
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2024-12-21 06:25:42 +0200

    initial commit

---
+++ build.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const drako = b.addExecutable(.{
+        .name = "drako",
+        .target = target,
+        .optimize = optimize,
+    });
+    drako.linkLibCpp();
+    drako.force_load_objc = true;
+    drako.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "main.cpp",
+        },
+        .flags = &[_][]const u8{
+            "-Wall",
+        },
+        .root = b.path("src"),
+    });
+    b.installArtifact(drako);
+
+    const run_cmd = b.addRunArtifact(drako);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Drako!\n";
+    return 0;
+}
